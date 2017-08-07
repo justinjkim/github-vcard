623246ac9dd3894f28669c516200bef04174c79a
Justin Kim <jjkim928@gmail.com>
2017-08-07 13:24:10 -0400

    basic set up of fetch

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("talking");
+
+
+fetch("https://api.github.com/users/justinjkim").then(
+	function(response) {
+    if (response.status !== 200) {
+      console.log('Looks like there was a problem. Status Code: ' +
+        response.status);
+      return;
+    } else {
+    	response.json().then(function(data) {
+      	console.log(data.login);
+    	});
+    }
+    
+  } // end of response functions
+); // end of overall "fetch" funtion
+
+
+
