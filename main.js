82bef90e9cffc8fe039af923aa190dc72dd94a95
Justin Kim <jjkim928@gmail.com>
2017-08-07 15:27:38 -0400

    added zigzag border and other stuff, unable to fetch email

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-fetch("https://randomuser.me/api/?results=1").then(
+fetch("https://api.github.com/users/justinjkim").then(
 	function(response) {
     if (response.status !== 200) {
       console.log('Looks like there was a problem. Status Code: ' +
         response.status);
       return;
-    } else {
+    } 
+    else {
     	response.json().then(function(githubAPI) {
 
     		let name = githubAPI.name;
@@ -20,38 +21,28 @@ fetch("https://randomuser.me/api/?results=1").then(
   			let basics =  `
   				<h1>The Basics</h1>
   				<ul>
-  					<li>Name: ${name}</li>
-  					<li>Github URL: <a href="${githubURL}">${login}</a></li>
-  					<li>Email: ${email}</li>
-  					<li>Company: ${company}</li>
-  					<li>Website: <a href="${blog}">${blog}</a></li>
+  					<li><span class="highlight">Name:</span> &nbsp; ${name}</li>
+  					<li><span class="highlight">Github URL:</span> &nbsp; <a href="${githubURL}">${login}</a></li>
+  					<li><span class="highlight">Email:</span> &nbsp; ${email}</li>
+  					<li><span class="highlight">Company:</span> &nbsp; ${company}</li>
+  					<li><span class="highlight">LinkedIn:</span> &nbsp; <a href="${blog}">${blog}</a></li>
   				</ul>
   			`
 
   			let story = `
   				<h1>The Story</h1>
   				<p>${bio}</p>
-  			`
-
-  			let picture = `
   				<img src="${avatar}" alt="picture" />
   			`
 
+
   			let sectionBasics = document.getElementById("basics");
   			let sectionStory = document.getElementById("story");
-  			let sectionPicture = document.getElementById("picture");
       	sectionBasics.innerHTML = basics;
       	sectionStory.innerHTML = story;
-      	sectionPicture.innerHTML = picture;
 
-
-
-
-
-
-
-    	}); // end of 200 response function
-    }
+    	}); // end of JSON parser function
+    } // end of 200 response function
     
   } // end of overall response functions
 ); // end of overall "fetch" funtion
